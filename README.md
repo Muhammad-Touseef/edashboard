d1cfe89bdaafc3bfae1dce8ed255ad8fc5b5994a
Muhammad-Touseef <touseefjavaid4@gmail.com>
2021-10-07 17:30:59 +0500

    added drag & drop functionality on permit board page

---
+++ README.md
@@ -0,0 +1,4 @@
+# dashboard
+Internship project - Temporary repo - WIP
+
+published at https://somia-ansari.github.io/dashboard/
---
+++ js/drag_drop.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+	$(".mozgat").draggable({
+	  helper: "clone",
+	  appendTo: "body",
+	  revert: "invalid",
+	  snap: ".tapad",
+	  stack: ".mozgat",
+	  scroll: false
+	});
+	$("#ch_dndBoard1").droppable({
+		cursor: "move",
+	  accept: ".mozgat",
+	  activeClass: "snaptarget-hover",
+	  drop: function (event, ui) {
+	    var ct = $(this);
+	    var item = $(ui.draggable);
+	    var origPos;
+	    var ctPos = ct.offset();
+
+	    if (item.is('.tapad')) {
+	      origPos = item.offset();
+	      ct.append(item);
+	    } else {
+	      origPos = ui.offset;
+	      item = item.clone();
+	      ct.append(item);
+	      item.removeClass("ui-draggable");
+	      item.addClass('tapad');
+	      item.draggable({
+	        containment: "#dndBoard",
+	        snap: ".tapad",
+	        stack: ".mozgat",
+	        scroll: false
+	      });
+	item.on('dragend',function(){
+	alert(item.getPosition().x+"/"+item.getPosition().y);
+	});
+	item.resizable();
+	item.addClass('remove');
+	var ex = $('<a href="Javascript:void(0)" class="delete" title="Remove">X</a>').css({
+	'position': 'absolute',
+	'top':-17,
+	'right': 6,
+	'height': 10,
+	'width': 10,
+	'background-color': 'transparent'
+	});
+	$(ex).insertAfter($(item.find('p')));
+	item.appendTo('#droppable');
+	$('.delete').on('click', function () {
+    $(this).parent('span').remove();
+  });
+	var ro = $('<div class="rotator"></div>').addClass('handler').css({
+	'position': 'absolute',
+	'top':-12,
+	'left': 3,
+	'height': 10,
+	'width': 10,
+	'background-color': 'black'
+	});
+	$(ro).insertAfter($(item.find('p')));
+  item.appendTo('#droppable');
+	applyRotation();
+	    }
+	    item.css({
+	      top: origPos.top - ctPos.top - 1,
+	      left: origPos.left - ctPos.left -1
+	    });
+	  }
+	});
+  $('#snaptarget3').droppable({
+    over: function(event, ui) {
+        ui.draggable.remove();
+    }
+  });  
+});
+function applyRotation() {
+  $('.handler').draggable({
+    opacity: 0.01,
+    helper: 'clone',
+    drag: function (event, ui) {
+      var rotateCSS = 'rotate(' + ui.position.left + 'deg)';
+
+      $(this).parent().css({
+        '-moz-transform': rotateCSS,
+        '-webkit-transform': rotateCSS
+      });
+    }
+  });
+}
