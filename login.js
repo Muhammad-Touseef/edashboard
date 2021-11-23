418e048d4039a7ae0118d4df2eea26d37b62677a
Muhammad-Touseef <58638308+Muhammad-Touseef@users.noreply.github.com>
2021-11-23 12:15:57 +0500

    Update login.js

--- login.js
+++ login.js
@@ -2,6 +2,11 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+function logincheck(){
+    if(localStorage.getItem("user") == null) {
+     window.location.replace("login.html");
+}
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
