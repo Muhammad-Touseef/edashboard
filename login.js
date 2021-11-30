77e7f3355f66576ce959fb885fab8d9b9c65454c
Muhammad-Touseef <58638308+Muhammad-Touseef@users.noreply.github.com>
2021-11-30 14:46:16 +0500

    Update login.js

--- login.js
+++ login.js
@@ -16,15 +16,4 @@ loginButton.addEventListener("click", (e) => {
         loginErrorMsg.style.opacity = 1;
     }
 })
-var hours = 1; // to clear the localStorage after 1 hour(if someone want to clear after 8hrs simply change hours=8)
-var now = new Date().getTime();
-var setupTime = localStorage.getItem('setupTime');
 
-if (setupTime == null) {
-    localStorage.setItem('setupTime', now)
-} else {
-    if(now-setupTime > hours*60*60*1000) {
-        localStorage.clear()
-        localStorage.setItem('setupTime', now);
-    }
-}
